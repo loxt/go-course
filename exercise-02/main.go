9d373ae048474abd99d6d8046469bbe6ed57b93b
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-14 02:52:44 -0400

    :sparkles: feat(): exercise nº 02

---
+++ exercise-02/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	x = 43
+	y = "James Bond"
+	z = true
+
+	fmt.Println(x, y, z)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
