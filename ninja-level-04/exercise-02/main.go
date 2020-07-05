b4f7c0e0c9e9aca40348d6af52821d30d2c43abd
loxt <emannuelloxt@hotmail.com>
2020-07-05 16:47:55 -0400

    :sparkles: feat(ninja-level-04): exercise 02

---
+++ ninja-level-04/exercise-02/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+
+	for i, v := range x {
+		fmt.Println(i, v)
+	}
+	fmt.Printf("%T\n", x)
+}
