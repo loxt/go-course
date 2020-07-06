0de3e738588cf815c43b7b664237e3d97c61fcfb
loxt <emannuelloxt@hotmail.com>
2020-07-06 00:16:58 -0400

    :sparkles: feat(ninja-level-04): exercise 08

---
+++ ninja-level-04/exercise-08/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james":         []string{"James bond", "James", "Bond"},
+		"Shaken not stirred": []string{"Shaken", "Sha", "Ken"},
+		"Martinis":           []string{"It's Martinis", "Its", "Mart"},
+		"Women":              []string{"That's a Women", "Thats", "Wom"},
+	}
+	fmt.Println(m)
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, v2 := range v {
+			fmt.Println("\t", i, v2)
+
+		}
+	}
+
+}
