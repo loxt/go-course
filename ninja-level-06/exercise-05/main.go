6603927c56194f8969dc1862d55191021eea1d3c
loxt <emannuelloxt@hotmail.com>
2020-09-18 16:28:16 -0400

    feat: add exercise 05

---
+++ ninja-level-06/exercise-05/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	length float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.length * s.length
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type shape interface {
+	area() float64
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}
+
+func main() {
+	c := circle{
+		radius: 12.345,
+	}
+
+	s := square{
+		length: 12,
+	}
+
+	info(c)
+	info(s)
+}
